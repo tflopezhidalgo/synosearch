8322ed3fdccf741dbfb88d9a49f5f10a3e4d9358
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-06-21 20:32:45 -0300

    debug

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,48 @@ use parsing::{
 
 
 use std::thread::{self, JoinHandle};
+use std::{time, vec};
 use std_semaphore::Semaphore;
-use std::sync::Arc;
-use std::collections::VecDeque;
+use std::sync::{Arc, Mutex, Condvar};
 
 
-fn pagina(w: Arc<String>, id: i32) -> () {
-    println!("Buscando sinónimos de {:?} en página {:?}", w, id);
+fn pagina(w: Arc<String>, id: i32, sem: Arc<Semaphore>, condvar: Arc<(Mutex<std::time::Instant>, Condvar)>) -> () {
+    sem.acquire();
+
+    let (lock, cvar) = &*condvar;
+    let mut last = cvar.wait_while(
+        lock.lock().unwrap(), 
+        |last| { 
+            let now = time::Instant::now();
+            println!("hey, {}", id);
+            now.duration_since(*last).as_secs() < 10
+        }).unwrap();
+
+    println!(" ----> haciendo request para sinónimos de {:?} en página {:?}", w, id);
+    let duration = time::Duration::from_millis(0);
+
+    thread::sleep(duration);
+    sem.release();
+
+    *last = time::Instant::now();
+
+    cvar.notify_all();
+
+    println!("Devolviendo ({:?})...", id);
 }
 
-fn palabra(w: Arc<String>) {
+fn palabra(w: Arc<String>, sem: Arc<Semaphore>, condvar: Arc<(Mutex<std::time::Instant>, Condvar)>) {
     println!("Buscando sinónimos para palabra: {:?}", w);
 
     let mut paginas : Vec<JoinHandle<()>> = vec!();
 
     for i in 0..2 {
         let w = w.clone();
+        let sem = sem.clone();
+        let condvar = condvar.clone();
         paginas.push(
             thread::spawn(move || {
-                pagina(w, i);
+                pagina(w, i, sem, condvar);
             })
         );
     }
@@ -37,21 +60,17 @@ fn palabra(w: Arc<String>) {
 }
 
 fn main() {
+    let sem = Arc::new(Semaphore::new(3));
 
-    let p1 = &ThesaurusProvider {url: "".to_string()};
-    let p2 = &YourDictionaryProvider {url: "".to_string()};
-    let p3 = &MarianWebsterProvider {url: "".to_string()};
-
-    let providers: Vec<& dyn Parser> = vec![p1, p2, p3];
-
-    for p in providers {
-        println!("{:?}", p.parse("car".to_string()));
-    }
-    //let sem = Arc::new(Semaphore::new(3));
+    let pair = Arc::new((Mutex::new(time::Instant::now()), Condvar::new()));
 
     let words = Arc::new(vec!(
         "palabra 1".to_string(),
-        "palabra 2".to_string()
+        "palabra 2".to_string(),
+        "palabra 3".to_string(),
+        "palabra 4".to_string(),
+        "palabra 5".to_string(),
+        "palabra 6".to_string(),
     ));
 
     let mut w_threads : Vec<JoinHandle<()>> = vec!();
@@ -59,9 +78,11 @@ fn main() {
     for i in 0..words.len() {
 
         let current_w = Arc::new(words[i].clone());
+        let sem = sem.clone();
+        let pair2 = pair.clone();
 
         w_threads.push(thread::spawn(move || {
-            palabra(current_w.clone());
+            palabra(current_w.clone(), sem, pair2);
         }));
     }
 
