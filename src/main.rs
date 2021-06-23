36e95e6158079dbef1ff69fd2c686b5e22742da6
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-06-23 01:54:19 -0300

    move parser to same module, use polimorphism

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,21 @@
-//mod parsing; 
-#[path = "parsing/parse_thesaurus.rs"] mod parse_thesaurus;
-#[path = "parsing/parse_merian_webster.rs"] mod parse_merian_webster;
-#[path = "parsing/parse_your_dictionary.rs"] mod parse_your_dictionary;
+mod parsing;
+use parsing::{
+    ThesaurusProvider, 
+    YourDictionaryProvider, 
+    MarianWebsterProvider, 
+    Parser
+};
 
-//use parsing::{DummyProvider, Parser};
-/* cantidad máxima de pedidos a webs de forma concurrente */
-//const MAX_REQ_CONCURRENCY: u32 = 1;
-
-/* TODO tomar params desde línea de comandos */
-/* TODO tiempo de espera entre dos requests consecutivas al mismo sitio */
 
 fn main() {
-    /*
-    let temp_1 = DummyProvider{
-        url: String::from("http://google.com")
-    };
-    let temp_2 = DummyProvider{
-        url: String::from("http://yahoo.com")
-    };
 
-    let providers: Vec<&Parser> = std::vec![&temp_1, &temp_2];
+    let p1 = &ThesaurusProvider {url: "".to_string()};
+    let p2 = &YourDictionaryProvider {url: "".to_string()};
+    let p3 = &MarianWebsterProvider {url: "".to_string()};
+
+    let providers: Vec<& dyn Parser> = vec![p1, p2, p3];
 
     for p in providers {
-        println!("{}", p.parse("car".to_string()));
-    }
-    */
-    let word = "table";
-    
-    let vec_th = parse_thesaurus::request_thesaurus(word);
-    let vec_mw = parse_merian_webster::request_merian_webster(word);
-    let vec_yd = parse_your_dictionary::request_your_dictionary(word);
-    
-    let mut vec = Vec::new();
-    vec.extend_from_slice(&vec_th);
-    vec.extend_from_slice(&vec_mw);
-    vec.extend_from_slice(&vec_yd);
-
-    for i in vec {
-        println!("{}", i);
+        println!("{:?}", p.parse("car".to_string()));
     }
 }
--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -1,14 +1,97 @@
+use reqwest::header::USER_AGENT;
+const APP_USER_AGENT: &str = "curl/7.68.0";
+
 pub trait Parser {
-    fn parse(&self, target: String) -> String;
+    fn parse(&self, target: String) -> Vec<String>;
 }
 
-/* TODO. refactor a módulos separados */
-pub struct DummyProvider {
-    pub url: String,
+/* -- theaurus -- */
+
+pub struct ThesaurusProvider {
+    pub url: String
 }
 
-impl Parser for DummyProvider {
-    fn parse(&self, _target: String) -> String {
-        return "fake synonym".to_string();
+impl Parser for ThesaurusProvider {
+    fn parse(&self, target: String) -> Vec<String> {
+        let url = format!("https://www.thesaurus.com/browse/{}", target);
+        let contents = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+        let vec_class = contents.split("e1ccqdb60\">").collect::<Vec<&str>>();
+        let vec_ul = vec_class[1].split("</ul>").collect::<Vec<&str>>();
+        let vec_il = vec_ul[0].split("<!-- -->").collect::<Vec<&str>>();
+
+        let mut vec = Vec::new();
+        for s in vec_il {
+            if s.contains("eh475bn0\">") {
+                let vec_data = s.split("eh475bn0\">").collect::<Vec<&str>>();
+                let target = vec_data[1].replace("%20", " ").replace("&#x27;", "'");
+                vec.push(target);
+            }
+        }
+        return vec;
+    }
+}
+
+
+/* -- yourdictonary -- */
+
+pub struct YourDictionaryProvider {
+    pub url: String
+}
+
+impl Parser for YourDictionaryProvider {
+    fn parse(&self, target: String) -> Vec<String> {
+        let client = reqwest::blocking::Client::new();
+        let url = format!("https://thesaurus.yourdictionary.com/{}", target);
+        let res = client.get(url)
+            .header(USER_AGENT, APP_USER_AGENT)
+            .send();
+
+        let contents = res.unwrap().text().unwrap();
+        let vec_class = contents.split("<div class=\"single-synonym-wrapper\" ").collect::<Vec<&str>>();
+        let vec_ul = vec_class[1].split("</span></button></div></div></div> <!----></div></div> <!----></div></div>").collect::<Vec<&str>>();
+        let vec_span = vec_ul[0].split("<!---->").collect::<Vec<&str>>(); 
+
+        let mut vec = Vec::new();
+        for s in vec_span {
+            if s.contains("class=\"synonym-link\" data-v-b5c08d74>") {
+                let split_word = s.split("class=\"synonym-link\" data-v-b5c08d74>").collect::<Vec<&str>>();
+                let split_link = split_word[1].split("</").collect::<Vec<&str>>();
+                vec.push(split_link[0].to_string());
+            }
+        }
+        return vec;
+    }
+}
+
+
+/* -- marian webster -- */
+
+pub struct MarianWebsterProvider {
+    pub url: String
+}
+
+impl Parser for MarianWebsterProvider {
+    fn parse(&self, target: String) -> Vec<String> {
+        let url = format!("https://www.merriam-webster.com/thesaurus/{}", target);
+        let contents = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+        let vec_class = contents.split("<ul class=\"mw-list\">").collect::<Vec<&str>>();
+        let vec_ul = vec_class[1].split("</ul>").collect::<Vec<&str>>();
+        let vec_il = vec_ul[0].split("<li>").collect::<Vec<&str>>();
+
+        let mut vec = Vec::new();
+        for s in vec_il {
+            let data = s.replace("\n", "");
+            let vec_data = data.split("\">").collect::<Vec<&str>>();
+
+            if vec_data[0].contains("<a class=\"\" href=\"/thesaurus/") {
+                let word = vec_data[0].replace("<a class=\"\" href=\"/thesaurus/", "")
+                    .replace(" ", "").replace("%20", " ");
+                //println!("{}", _word);
+                vec.push(word);
+            }
+        }
+        return vec;
     }
 }
