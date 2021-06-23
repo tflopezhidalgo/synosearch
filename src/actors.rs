700c02c10794e3cd00df963844bc82eaeb151423
Elianagam <elianagam2@gmail.com>
2021-06-23 14:12:38 -0300

    Fix error actors

--- src/actors.rs
+++ src/actors.rs
@@ -11,7 +11,7 @@ use parsing::{
 // we have to define the response type (rtype)
 #[derive(Message)]
 #[rtype(result = "Vec<String>")]
-struct Parse(String);
+struct Word(String);
 
 // Actor definition
 struct Scraper;
@@ -20,19 +20,19 @@ impl Actor for Scraper {
     type Context = Context<Self>;
 }
 
-impl Handler<Parse> for Scraper {
+impl Handler<Word> for Scraper {
     type Result = Vec<String>; // <- Message response type
 
-    fn handle(&mut self, word: String, _ctx: &mut Context<Self>) -> Self::Result {
+    fn handle(&mut self, word: Word, _ctx: &mut Context<Self>) -> Self::Result {
         let p3 = &MarianWebsterProvider {url: "".to_string()};
-        return p3.parse(word);
+        return p3.parse(word.0);
     }
 }
 
 #[actix::main] // <- starts the system and block until future resolves
-async fn actors_main() {
+async fn main() {
     let addr = Scraper.start();
-    let res = addr.send("car".to_string()).await; // <- send message and get future for result
+    let res = addr.send(Word("car".to_string())).await; // <- send message and get future for result
 
     match res {
         Ok(result) => println!("WORD: {:?}", result),
