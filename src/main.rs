66395df47c45ba0d69013f1044acc44aac4a71c4
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-06-24 20:11:26 -0300

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,108 @@
 mod parsing;
+use core::time;
+use std::thread::sleep;
+use std::sync::Arc;
+
 use parsing::{
-    //ThesaurusProvider, 
-    //YourDictionaryProvider, 
     MarianWebsterProvider, 
+    ThesaurusProvider,
     Parser
 };
 
-use actix::{Actor, Context, System};
+use actix::{Actor, Context, System, SyncContext};
+use actix::prelude::*;
 
-struct MyActor{
-    word: String,
+/* - Actor leyendo las palabras del archivo.
+ *  - Actor que recibe una palabra a buscar.
+ *  - Tres instancias únicas de sitios (gatekeeper)
+ *  - Pool de workers.
+ */
+
+/* Messages */
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct Msg {
+    msg: Arc<String>,
 }
 
-impl Actor for MyActor {
-    type Context = Context<Self>;
+#[derive(Message)]
+#[rtype(result = "Vec<String>")]
+struct ParsingMessage {
+    msg: Arc<String>,
+}
+
+/* Actors */
+
+struct TheaurusWorker { }
+
+// TODO. sacarlo
+impl Actor for TheaurusWorker {
+    type Context = SyncContext<Self>;
 
     fn started(&mut self, _ctx: &mut Self::Context) {
-        let p3 = &MarianWebsterProvider {url: "".to_string()};
-        println!("{:?}",p3.parse(self.word.clone()));        
-        System::current().stop(); // <- stop system
+        return
     }
 }
+
+impl Handler<ParsingMessage> for TheaurusWorker {
+
+    type Result = Vec<String>;
+
+    fn handle(&mut self, msg: ParsingMessage, _ctx: &mut SyncContext<Self>) -> Self::Result {
+        /* Busca sinónimo para una palabra en un determinado sitio */
+        let worker = &ThesaurusProvider { url: "".to_string() };
+
+        let tmp = (*msg.msg).clone();
+        return worker.parse(tmp);
+    }
+}
+
+struct WordGateKeeper {
+    //FIXME
+    worker: Arc<Addr<TheaurusWorker>>
+}
+
+// TODO. sacarlo
+impl Actor for WordGateKeeper {
+    type Context = Context<Self>;
+    fn started(&mut self, _ctx: &mut Self::Context) {}
+}
+
+impl Handler<Msg> for WordGateKeeper {
+
+    type Result = ();
+
+    fn handle(&mut self, msg: Msg, _ctx: &mut Context<Self>) -> Self::Result {
+        self.worker.do_send(ParsingMessage{ msg: msg.msg });
+    }
+}
+
 
 fn main() {
     let system = System::new();
-    let word = "car".to_string();
-    let _addr = system.block_on(async { MyActor{word: word}.start() });
 
-    //system.run(); 
+    let mut words = vec!();
+
+    let w1 = Arc::new("casa".to_string());
+    let w2 = Arc::new("auto".to_string());
+    let w3 = Arc::new("perro".to_string());
+
+    words.push(w1.clone());
+    words.push(w2.clone());
+    words.push(w3.clone());
+
+    let thesaurus_worker = Arc::new(SyncArbiter::start(5, || TheaurusWorker{}));
+
+    let t_worker = thesaurus_worker.clone();
+
+    let gatekeeper = WordGateKeeper { worker: t_worker }.start();
+
+    system.block_on(async {
+        for w in words {
+            gatekeeper.do_send(Msg{ msg: w.clone() });
+        }
+    });
+
+    system.run().unwrap(); 
 }
-
-/*
-fn main() {
-
-    let p1 = &ThesaurusProvider {url: "".to_string()};
-    let p2 = &YourDictionaryProvider {url: "".to_string()};
-    let p3 = &MarianWebsterProvider {url: "".to_string()};
-
-    let providers: Vec<& dyn Parser> = vec![p1, p2, p3];
-
-    for p in providers {
-        println!("{:?}", p.parse("car".to_string()));
-    }
-}
-*/
