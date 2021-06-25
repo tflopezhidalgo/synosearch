8126a8b68f0fb0d2f9c89974de4cacaf8fc66be5
Elianagam <elianagam2@gmail.com>
2021-06-25 00:31:25 -0300

    Add funcion read file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 mod parsing;
+
+use std::process;
+use std::env;
+use std::fs;
+
 use parsing::{
     ThesaurusProvider, 
     YourDictionaryProvider, 
@@ -7,8 +12,7 @@ use parsing::{
 };
 
 
-fn main() {
-
+fn try_mode() {
     let p1 = &ThesaurusProvider {url: "".to_string()};
     let p2 = &YourDictionaryProvider {url: "".to_string()};
     let p3 = &MarianWebsterProvider {url: "".to_string()};
@@ -18,4 +22,37 @@ fn main() {
     for p in providers {
         println!("{:?}", p.parse("car".to_string()));
     }
+
+}
+
+fn read_file_into_vector(filename: String) -> Vec<String> {
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file"); 
+
+    let words = contents.split("\n").collect::<Vec<&str>>();
+    let mut vec = Vec::new();
+    for w in words.into_iter() {
+        vec.push(w.to_string());
+    }
+    return vec;
+}
+
+fn choose_mode(mode:String, word: String) {
+    if mode.eq("actors") {
+        println!("actors");
+    } else if mode.eq("threads") {
+        println!("threads");
+        read_file_into_vector(word);
+    } else {
+        try_mode();
+    }
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        process::exit(-1);
+    }
+    choose_mode(args[0].clone(), args[1].clone());
 }
