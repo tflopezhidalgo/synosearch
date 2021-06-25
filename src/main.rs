1947e1d4da8cbec826c13a465ad4d19f47603019
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-06-25 01:58:25 -0300

    fix (?)

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 mod parsing;
-//use core::time;
-//use std::thread::sleep;
+
 use std::sync::Arc;
+use actix::prelude::*;
+use actix::{Actor, Context, System, SyncContext};
+use std::time::Instant;
 
 use parsing::{
-  //  MarianWebsterProvider, 
     ThesaurusProvider,
+    MarianWebsterProvider,
+    YourDictionaryProvider,
     Parser
 };
 
-use actix::{Actor, Context, System, SyncContext};
-use actix::prelude::*;
-
-/* - Actor leyendo las palabras del archivo.
- *  - Actor que recibe una palabra a buscar.
- *  - Tres instancias únicas de sitios (gatekeeper)
- *  - Pool de workers.
- */
-
 /* Messages */
+#[derive(Message)]
+#[rtype(result = "()")]
+struct AddrMsg {
+    msg: Arc<String>,
+    source_addr: Addr<PerWordWorker>
+}
 
 #[derive(Message)]
 #[rtype(result = "()")]
@@ -26,57 +26,128 @@ struct Msg {
     msg: Arc<String>,
 }
 
+
 #[derive(Message)]
-#[rtype(result = "Vec<String>")]
-struct ParsingMessage {
+#[rtype(result = "()")]
+struct AddrMsg_ {
     msg: Arc<String>,
+    source_addr: Arc<Addr<PerWordWorker>>
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct SVec {
+    web: Arc<String>,
+    msg: Arc<Vec<String>>,
 }
 
 /* Actors */
 
-struct TheaurusWorker { }
+struct TheaurusWorker;
 
 impl Actor for TheaurusWorker {
     type Context = SyncContext<Self>;
 }
 
-impl Handler<ParsingMessage> for TheaurusWorker {
+impl Handler<AddrMsg_> for TheaurusWorker {
 
-    type Result = Vec<String>;
+    type Result = ();
 
-    fn handle(&mut self, msg: ParsingMessage, _ctx: &mut SyncContext<Self>) -> Self::Result {
+    fn handle(&mut self, msg: AddrMsg_, _ctx: &mut SyncContext<Self>) -> Self::Result {
         /* Busca sinónimo para una palabra en un determinado sitio */
         let worker = &ThesaurusProvider { url: "".to_string() };
 
         let tmp = (*msg.msg).clone();
-        let syn = worker.parse(tmp);
-        println!("WORD: {:?}\n{:?}", msg.msg, syn);
-        return syn;
+        let syn = Arc::new(worker.parse(tmp));
+        msg.source_addr.do_send(SVec{ msg: syn, web: Arc::new("Theaurus".to_string()) });
+    }
+}
+
+struct YourDictionaryWorker;
+
+impl Actor for YourDictionaryWorker {
+    type Context = SyncContext<Self>;
+}
+
+impl Handler<AddrMsg_> for YourDictionaryWorker {
+
+    type Result = ();
+
+    fn handle(&mut self, msg: AddrMsg_, _ctx: &mut SyncContext<Self>) -> Self::Result {
+        /* Busca sinónimo para una palabra en un determinado sitio */
+        let worker = &YourDictionaryProvider { url: "".to_string() };
+
+        let tmp = (*msg.msg).clone();
+        let syn = Arc::new(worker.parse(tmp));
+        msg.source_addr.do_send(SVec{ msg: syn, web: Arc::new("YourDictonary".to_string()) });
+    }
+}
+
+struct MarianWebsterWorker;
+
+impl Actor for MarianWebsterWorker {
+    type Context = SyncContext<Self>;
+}
+
+impl Handler<AddrMsg_> for MarianWebsterWorker {
+
+    type Result = ();
+
+    fn handle(&mut self, msg: AddrMsg_, _ctx: &mut SyncContext<Self>) -> Self::Result {
+        /* Busca sinónimo para una palabra en un determinado sitio */
+        let worker = &MarianWebsterProvider { url: "".to_string() };
+
+        let tmp = (*msg.msg).clone();
+        let syn = Arc::new(worker.parse(tmp));
+        msg.source_addr.do_send(SVec{ msg: syn, web: Arc::new("Marian".to_string()) });
     }
 }
 
 struct WordGateKeeper {
-    worker: Arc<Addr<TheaurusWorker>>, 
-    qty_words: usize
+    //TODO. polymorphic vector
+    t_worker: Arc<Addr<TheaurusWorker>>, 
+    y_worker: Arc<Addr<YourDictionaryWorker>>, 
+    m_worker: Arc<Addr<MarianWebsterWorker>>, 
 }
 
 impl Actor for WordGateKeeper {
     type Context = Context<Self>;
 }
 
-impl Handler<Msg> for WordGateKeeper {
+impl Handler<AddrMsg> for WordGateKeeper {
+    type Result = ();
+
+    fn handle(&mut self, msg: AddrMsg, _ctx: &mut Context<Self>) -> Self::Result {
+        let source_addr = Arc::new(msg.source_addr);
+
+        self.t_worker.do_send(AddrMsg_{ source_addr: source_addr.clone(), msg: msg.msg.clone() });
+        self.y_worker.do_send(AddrMsg_{ source_addr: source_addr.clone(), msg: msg.msg.clone() });
+        self.m_worker.do_send(AddrMsg_{ source_addr: source_addr.clone(), msg: msg.msg.clone() });
+    }
+}
+
+struct PerWordWorker {
+    gate_keeper: Arc<Addr<WordGateKeeper>>,
+}
+
+impl Actor for PerWordWorker {
+    type Context = Context<Self>;
+}
+
+impl Handler<Msg> for PerWordWorker {
     type Result = ();
 
     fn handle(&mut self, msg: Msg, _ctx: &mut Context<Self>) -> Self::Result {
-        if self.qty_words < 1 {
-            println!("salir: {:?}", self.qty_words);
-            System::current().stop();
-        } else {
-            println!("msg: {:?} - {:?}", msg.msg, self.qty_words);
-            let syn = self.worker.do_send(ParsingMessage{ msg: msg.msg });
-            println!("return {:?}", syn);
-            self.qty_words -= 1;
-        } 
+        println!("Asking synonym for {:?}", msg.msg);
+        self.gate_keeper.do_send(AddrMsg{ source_addr: _ctx.address(), msg: msg.msg })
+    }
+}
+
+impl Handler<SVec> for PerWordWorker {
+    type Result = ();
+
+    fn handle(&mut self, msg: SVec, _ctx: &mut Context<Self>) -> Self::Result {
+        println!("{:p} recibio {:?}", self, msg.msg.len());
     }
 }
 
@@ -94,16 +165,27 @@ fn main() {
     words.push(w2.clone());
     words.push(w3.clone());
     
-    let thesaurus_worker = Arc::new(SyncArbiter::start(5, || TheaurusWorker{}));
-    let t_worker = thesaurus_worker.clone();
-
     system.block_on(async {
-        let gatekeeper = WordGateKeeper { worker: t_worker, qty_words: 3 }.start();
+        let thesaurus_worker = Arc::new(SyncArbiter::start(5, || TheaurusWorker{}));
+        let marian_worker = Arc::new(SyncArbiter::start(5, || MarianWebsterWorker{}));
+        let your_dict_worker = Arc::new(SyncArbiter::start(5, || YourDictionaryWorker{}));
+
+        let gatekeeper = Arc::new(
+            WordGateKeeper {
+                t_worker: thesaurus_worker.clone(),
+                y_worker: your_dict_worker.clone(),
+                m_worker: marian_worker.clone()
+            }
+            .start());
+
+        let per_word_worker = PerWordWorker { gate_keeper: gatekeeper.clone() }.start();
 
         for w in words {
-            gatekeeper.do_send(Msg{ msg: w.clone() });
+            per_word_worker.do_send(Msg{ msg: w.clone() });
         }
     });
 
-    system.run(); 
+    system.run().unwrap();
+
+    System::current().stop();
 }
