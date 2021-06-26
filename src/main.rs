20885d040e2bfc8372eb56e494d41ee44f3d05bd
Elianagam <elianagam2@gmail.com>
2021-06-25 21:28:13 -0300

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod parsing;
+/*mod parsing;
 use parsing::{
     ThesaurusProvider, 
     YourDictionaryProvider, 
@@ -17,5 +17,49 @@ fn main() {
 
     for p in providers {
         println!("{:?}", p.parse("car".to_string()));
+    }
+}
+*/
+
+use std::io::Write;
+use std::{thread};
+use std::fs::File;
+use std::sync::{Arc, Mutex};
+
+
+fn main() {
+    let f = File::create("foo.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem creating the file: {:?}", error)
+    };
+    let logger = Arc::new(Mutex::new(f));
+
+    let mut threads = Vec::new();
+    for _ in 0..6 {
+       let file = Arc::clone(&logger);
+
+        let thread = thread::spawn(move || {
+            let mut file = match file.lock() {
+                Ok(m) => m,
+                Err(error) => panic!("Problem lock file: {:?}", error)
+            };
+            match file.write_all(b"Hola mundo\n") {
+                Ok(m) => m,
+                Err(error) => panic!("Problem creating the file: {:?}", error)
+            };
+
+
+        });
+        threads.push(thread);
+    }
+
+    for thread in threads {
+        let r = thread.join();
+        match r {
+                Ok(m) => m,
+                Err(error) => panic!("Problem creating the file: {:?}", error)
+            };
     }
 }
