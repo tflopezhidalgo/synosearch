53097e942ff80e581e875d0353195c1d8b47e0b4
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-06-27 00:11:23 -0300

    add actors/messages modules left in previous commit

---
+++ src/messages.rs
@@ -0,0 +1,30 @@
+use std::sync::Arc;
+use actix::prelude::*;
+use crate::actors::*;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct SynonymRequest {
+    pub target: Arc<String>,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct GatekeeperRequest {
+    pub target: Arc<String>,
+    pub response_addr: Arc<Addr<PerWordWorker>>,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct WorkerSynonymsRequest {
+    pub target: Arc<String>,
+    pub response_addr: Arc<Addr<PerWordWorker>>,
+    pub parser_key: String,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct SynonymsResult {
+    pub synonyms: Arc<Vec<String>>,
+}
