cecc6f4eeb7979411bbdb80531effdf940d4a73d
Elianagam <elianagam2@gmail.com>
2021-07-04 20:51:06 -0300

    File read line by line

--- src/utils/file_reader.rs
+++ src/utils/file_reader.rs
@@ -1,10 +1,9 @@
+use std::fs::File;
 use std::sync::Arc;
-use std::fs;
-use std::io;
+use std::io::{BufReader, BufRead, Error};
 
 use crate::Logger;
 
-const SPLIT_CHAR: &str = "\n";
 const MESSAGE_INIT: &str = "Read file with words";
 const MESSAGE_SPLIT: &str = "Split file into vector";
 const MESSAGE_RETURN: &str = "Return vectors of words";
@@ -24,20 +23,18 @@ impl FileReader {
     /// Returns a result that can be a vector of words from
     /// `self.filename` splitting by newline separator or a
     /// io::Error.
-    pub fn get_words(&self) -> Result<Vec<String>, io::Error> {
+    pub fn get_words(&self) -> Result<Vec<String>, Error> {
         self.logger.info(MESSAGE_INIT.to_string());
 
-        let contents = match fs::read_to_string(&self.filename) {
-            Ok(content) => content,
-            Err(e) => return Err(e),
-        };
+        let file = File::open(&self.filename)?;
+        let reader = BufReader::new(file);
+
+        let mut words = vec![];
+        for line in reader.lines() {
+            words.push(line?.to_string());
+        }
 
         self.logger.info(MESSAGE_SPLIT.to_string());
-        let mut words = contents
-            .split(SPLIT_CHAR)
-            .map(|word| word.to_string())
-            .collect::<Vec<String>>();
-
         words.retain(|x| (x != "" && x != " "));
 
         self.logger.info(MESSAGE_RETURN.to_string());
